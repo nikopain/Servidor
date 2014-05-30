a3a9e33977468e417b386eb8ea83a8d302853509
nikopain <nicolas.curapil@alumnos.usm.cl>
2014-05-29 23:05:09 -0400

    Servidor Corriendo con thread

--- Serv/src/serv/Serv.java
+++ Serv/src/serv/Serv.java
@@ -10,29 +10,55 @@ package serv;
  *
  * @author niko
  */
-
 import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
- 
+import client.Client; 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-public class Serv {
-
+public class Serv implements Runnable {
     public static int port = 4000;
-    
-    public static void main(String[] args) throws IOException {
-        //Servidor en espera de clientes
+    Socket client;
+    private static int count=0;
+    private int ID;
+    Serv(Socket socket, int id) {
+            this.client = socket;
+            this.ID = id;
+    }
+ 
+    public static void main(String argv[]) throws Exception{ 
+                 
         try{
-            ServerSocket s = new ServerSocket(port);
-            while (true){
-                Socket cl= s.accept();
-                Clientes Tcl= new Clientes(cl);
-                Tcl.start();
-            }
-        }catch(IOException e){
-            System.out.println( e );
+            ServerSocket Server = new ServerSocket(port);
+            System.out.println("ServerSocket Initialized");
+            while(true){             
+                Socket client = Server.accept();
+                Runnable runa= new Serv(client,++count);
+                Thread t= new Thread(runa);
+                t.start();    
+            }  
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    } 
+
+    @Override
+    public void run() {
+        String clientSentence;          
+        String capitalizedSentence; 
+	try {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));             
+            DataOutputStream outToClient = new DataOutputStream(client.getOutputStream());             
+            clientSentence = inFromClient.readLine();             
+            System.out.println("Received from client "+ID+": " + clientSentence);             
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';             
+            outToClient.writeBytes(capitalizedSentence);      
+        }
+        catch(Exception e){
+            System.out.println(e);
         }
     }
     
